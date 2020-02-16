e5c0761dc0577fe5b80f554ca3bb01e4958b174d
Teacher <teacher@Bennys-MacBook-Pro-2.local>
2020-02-16 21:06:11 +0200

    first commit

---
+++ DemoProjectiOS/DemoProjectiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoProjectiOS
+//
+//  Created by Teacher on 16/02/2020.
+//  Copyright © 2020 hackeru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
